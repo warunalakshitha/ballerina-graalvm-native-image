9f635c1da10d33df6a9910cc0441a397cc6628bd
Waruna Lakshitha <warunalakshitha@users.noreply.github.com>
2022-08-17 10:41:45 +0530

    Update native-image.sh

--- native-image.sh
+++ native-image.sh
@@ -7,7 +7,7 @@ native-image -H:MaxDuplicationFactor=40.0 \
        -jar $EXECUTABLE_FILE --no-fallback \
        -H:IncludeResources=logging.properties \
         -H:ReflectionConfigurationFiles=reflection-config.json \
-       --enable-url-protocols=http,https,ws \
+       --enable-url-protocols=http,https \
        --initialize-at-build-time=org.slf4j.impl.JDK14LoggerAdapter,org.slf4j.LoggerFactory,org.slf4j.impl.StaticLoggerBinder \
        --initialize-at-run-time=io.netty.handler.ssl.OpenSsl,io.netty.handler.codec.http2.Http2ServerUpgradeCodec,io.netty.handler.codec.http.HttpMethods,io.netty.handler.codec.compression.ZstdOptions \
        --initialize-at-run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils,io.netty.handler.codec.http2.HttpConversionUtil,io.netty.handler.codec.http2.HttpConversionUtil$Http2ToHttpHeaderTranslator \
